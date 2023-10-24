27b9f310d8a1dcdbe5580a01924e7461b0220f47
JonasChai <jonas.chairutdinovas@gmail.com>
2023-10-24 10:48:24 +0300

    added remaining routes and controllers

--- controllers/prescriptions.js
+++ controllers/prescriptions.js
@@ -2,7 +2,14 @@ import db from "../db.js";
 
 const GET_PET_PRESCRIPTIONS = async (req, res) => {
     try {
-        const prescriptions = await db.query(`SELECT * FROM prescriptions WHERE pet_id = ${req.params.id}`);
+        const prescriptions = await db.query(`
+            SELECT *, pets.name AS pet_name, medications.name AS med_name FROM prescriptions
+            INNER JOIN pets
+                ON prescriptions.pet_id = pets.id
+            INNER JOIN medications
+                ON prescriptions.medication_id = medications.id
+            WHERE pet_id = ${req.params.id};
+        `);
         return res.json({ prescriptions: prescriptions.rows });
     } catch (err) {
         console.log("ERROR: ", err);
@@ -10,4 +17,16 @@ const GET_PET_PRESCRIPTIONS = async (req, res) => {
     }
 }
 
-export { GET_PET_PRESCRIPTIONS };
+const ADD_PET_PRESCRIPTION = async (req, res) => {
+    try {
+        const prescription = await db.query(`INSERT INTO prescriptions(
+            medication_id, pet_id, comment)
+            VALUES (${req.body.medication}, ${req.params.id}, '${req.body.comment}' )`)
+            return res.json({ prescription: prescription.rows });
+    } catch (err) {
+        console.log("ERROR: ", err);
+        res.status(500).json({ response: "something went wrong" });
+    }
+}
+
+export { GET_PET_PRESCRIPTIONS, ADD_PET_PRESCRIPTION };
--- routes/prescriptions.js
+++ routes/prescriptions.js
@@ -2,8 +2,11 @@ import express from "express";
 const router = express.Router();
 import {
     GET_PET_PRESCRIPTIONS,
+    ADD_PET_PRESCRIPTION
 } from "../controllers/prescriptions.js";
 
 router.get("/prescriptions/:id", GET_PET_PRESCRIPTIONS);
+
+router.post("/prescriptions/:id", ADD_PET_PRESCRIPTION);
 
 export default router;
